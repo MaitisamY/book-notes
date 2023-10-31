400fed4d2453c3e3cd5bcb863eb493f494aedaff
M aitisam Y <m.aitisamyaseen@gmail.com>
2023-10-31 21:50:25 +0500

    Created json file for book covers, reading the data from json file and applied some styles

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import fs from "fs";
 import pg from "pg";
+
+const db = new pg.Client({
+    user : "postgres",
+    database : "book-notes",
+    host : "localhost",
+    password : "my@postgres",
+    port : 5432,
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -9,8 +20,22 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended : true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+async function getBooks() {
+    
+}
+
+app.get("/", async (req, res) => {
+    fs.readFile("public/book-covers.json", "utf8", (error, data) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        else {
+            res.render("index.ejs", {
+                data : JSON.parse(data)
+            });
+        }
+    });
 });
 
 app.listen(port, () => {
